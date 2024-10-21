63951a74ab9c8a34c00aab3d054de9a4f44a2318
Alina <eparis@atlassian.com>
2024-10-21 13:44:44 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int initialAccount = 500;
+        int amountDeposit = 1500;
 
+        int bonus;
+        if (amountDeposit > 1000) {
+            bonus = amountDeposit / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int balance = initialAccount + amountDeposit + bonus;
+        System.out.println("На Вашем счете:" + balance +  "р.");
+        System.out.println("Бонус за пополнение:" + bonus +  "р.");
     }
 }
